ad89d1d887fdb4dac873bbb98eabf43926675f1d
oudom17 <oudom669293@gmail.com>
2025-02-17 10:27:13 +0700

    init

---
+++ cart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "include/head.php" ?>
+<body>
+    <!-- Header -->
+    <?php include "include/header.php" ?>
+    <!-- Close Header -->
+
+    <!-- Modal -->
+    <?php include "include/modal.php" ?>
+
+    
+
+</body>
+</html>
